3eded621c42514cf823026474ab1a78e75e8fc3a
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-08-08 17:34:04 +0500

    pull

--- src/components/Reyting/Reyting.jsx
+++ src/components/Reyting/Reyting.jsx
@@ -18,23 +18,69 @@ const Reyting = () => {
       });
       data.reverse();
     const columns = [
-        { title: "ID", dataIndex: "id", key: "id" },
-        { title: "Muassasa nomi", dataIndex: "nomi", key: "nomi" },
-        { title: "O`quv ishlari", dataIndex: "oquv_ishlari", key: "oquv_ishlari" },
-        { title: "Yoshlar masalalari", dataIndex: "yoshlar", key: "yoshlar" },
-        { title: "Ishlab chiqarish ta`limi", dataIndex: "ishlab_chiqarish", key: "ishlab_chiqarish" },
-        { title: "Moliyaviy ishalari", dataIndex: "moliyaviy", key: "moliyaviy" },
-        { title: "Xo`jalik ishlari", dataIndex: "xojalik", key: "xojalik" },
-        { title: "Ta`lim sifatini nazorat qilish", dataIndex: "talim_sifati", key: "talim_sifati" },
-        { title: "Ijro intizomi", dataIndex: "ijro_intizomi", key: "ijro_intizomi" },
-        { title: "Aniqlangan qoida buzarliklar", dataIndex: "jazo", key: "jazo" },
+        { title: "Muassasa nomi", dataIndex: "nomi", key: "nomi",
+        children: [
+          {
+            title: "Олиш мумкин бўлган максимал балл",
+            dataIndex: "nomi",
+            key: "nomi",
+          },
+        ], },
+        { title: "O`quv ishlari", dataIndex: "oquv_ishlari", key: "oquv_ishlari",
+        children: [
+          {
+            title: "15",
+            dataIndex: "oquv_ishlari",
+            key: "oquv_ishlari",
+          },
+        ], },
+        { title: "Yoshlar masalalari", dataIndex: "yoshlar", key: "yoshlar",
+        children: [
+          {
+            title: "15",
+            dataIndex: "yoshlar",
+            key: "yoshlar",
+          },
+        ], },
+        { title: "Ishlab chiqarish ta`limi", dataIndex: "ishlab_chiqarish", key: "ishlab_chiqarish",
+        children: [
+          { title: "15", dataIndex: "ishlab_chiqarish", key: "ishlab_chiqarish" },
+        ], },
+        { title: "Moliyaviy ishalari", dataIndex: "moliyaviy", key: "moliyaviy",
+        children: [{ title: "15", dataIndex: "moliyaviy", key: "moliyaviy" }], },
+        { title: "Xo`jalik ishlari", dataIndex: "xojalik", key: "xojalik" ,
+        children: [{ title: "10", dataIndex: "xojalik", key: "xojalik" }],},
+        { title: "Ta`lim sifatini nazorat qilish", dataIndex: "talim_sifati", key: "talim_sifati",
+        children: [
+          { title: "20", dataIndex: "talim_sifati", key: "talim_sifati" },
+        ], },
+        { title: "Ijro intizomi", dataIndex: "ijro_intizomi", key: "ijro_intizomi",
+        children: [
+          { title: "10", dataIndex: "ijro_intizomi", key: "ijro_intizomi" },
+        ], },
+        { title: "Aniqlangan qoida buzarliklar", dataIndex: "jazo", key: "jazo",
+        children: [{ title: "0", dataIndex: "jazo", key: "jazo" }], },
         {
           title: "1 oyda  to`plangan   umumiy ball",
           key: "umumiy",
+          children: [{ title: "100", dataIndex: "umumiy", key: "umumiy" }],
           dataIndex: "",
-          render: (text) => {
-            return <p>{parseInt(text.umumiy )} ball</p>;
-          },
+          children: [
+            {
+              title: "100.0",
+              dataIndex: "",
+              key: "Color",
+              render: (text) => {
+                if (text.umumiy < 50) {
+                  return <p className="red_error">Qoniqarsiz</p>;
+                } else if (text.umumiy > 80) {
+                  return <p className="alo_error">A`lo</p>;
+                } else if (text.umumiy < 80 || text.umumiy > 50) {
+                  return <p className="blue_error">Yaxshi</p>;
+                }
+              },
+            },
+          ],
         },
         {
           title: "Baholash turi",
