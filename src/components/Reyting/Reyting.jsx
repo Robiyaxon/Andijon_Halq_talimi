0a586dbcfcc025eb6f22314561fc9994b9afb444
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-08-09 19:49:54 +0500

    pull

--- src/components/Reyting/Reyting.jsx
+++ src/components/Reyting/Reyting.jsx
@@ -1,111 +1,147 @@
-import React, { useEffect, useState } from 'react'
-import {Table } from "antd";
+import React, { useEffect, useState } from "react";
+import { Table } from "antd";
 import { instance } from "../../api/API";
 import { Content } from "antd/lib/layout/layout";
-import style from "./Reyting.module.css"
-import { useTranslation } from 'react-i18next';
-import Column from 'antd/es/table/Column';
+import style from "./Reyting.module.css";
+import { useTranslation } from "react-i18next";
 
 const Reyting = () => {
-  const {t} =useTranslation()
-    const [data, setData] = useState([]);
-    useEffect(() => {
-      instance.get("ballash/").then((response) => setData(response.data));
-    }, []);
-    data.sort(function(a, b) {
-        return a.umumiy - b.umumiy;
-      });
-      data.reverse();
-    const columns = [
-        { title: "Muassasa nomi", dataIndex: "nomi", key: "nomi",
-        children: [
-          {
-            title: "Олиш мумкин бўлган максимал балл",
-            dataIndex: "nomi",
-            key: "nomi",
-          },
-        ], },
-        { title: "O`quv ishlari", dataIndex: "oquv_ishlari", key: "oquv_ishlari",
-        children: [
-          {
-            title: "15",
-            dataIndex: "oquv_ishlari",
-            key: "oquv_ishlari",
-          },
-        ], },
-        { title: "Yoshlar masalalari", dataIndex: "yoshlar", key: "yoshlar",
-        children: [
-          {
-            title: "15",
-            dataIndex: "yoshlar",
-            key: "yoshlar",
-          },
-        ], },
-        { title: "Ishlab chiqarish ta`limi", dataIndex: "ishlab_chiqarish", key: "ishlab_chiqarish",
-        children: [
-          { title: "15", dataIndex: "ishlab_chiqarish", key: "ishlab_chiqarish" },
-        ], },
-        { title: "Moliyaviy ishalari", dataIndex: "moliyaviy", key: "moliyaviy",
-        children: [{ title: "15", dataIndex: "moliyaviy", key: "moliyaviy" }], },
-        { title: "Xo`jalik ishlari", dataIndex: "xojalik", key: "xojalik" ,
-        children: [{ title: "10", dataIndex: "xojalik", key: "xojalik" }],},
-        { title: "Ta`lim sifatini nazorat qilish", dataIndex: "talim_sifati", key: "talim_sifati",
-        children: [
-          { title: "20", dataIndex: "talim_sifati", key: "talim_sifati" },
-        ], },
-        { title: "Ijro intizomi", dataIndex: "ijro_intizomi", key: "ijro_intizomi",
-        children: [
-          { title: "10", dataIndex: "ijro_intizomi", key: "ijro_intizomi" },
-        ], },
-        { title: "Aniqlangan qoida buzarliklar", dataIndex: "jazo", key: "jazo",
-        children: [{ title: "0", dataIndex: "jazo", key: "jazo" }], },
+  const { t } = useTranslation();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    instance.get("ballash/").then((response) => setData(response.data));
+  }, []);
+  data.sort(function (a, b) {
+    return a.umumiy - b.umumiy;
+  });
+  data.reverse();
+  const columns = [
+    {
+      title: "Muassasa nomi",
+      dataIndex: "nomi",
+      key: "nomi",
+
+      children: [
         {
-          title: "1 oyda  to`plangan   umumiy ball",
-          key: "umumiy",
-          children: [{ title: "100", dataIndex: "umumiy", key: "umumiy" }],
-          dataIndex: "",
-          render: (text) => {
-            return <p>{parseInt(text.umumiy )} ball</p>;
-          },
-          children: [{ title: "100", dataIndex: "umumiy", key: "umumiy" }],
+          title: "Maksimal Ballar",
+          dataIndex: "nomi",
+          key: "nomi",
         },
+      ],
+    },
+    {
+      title: "O`quv ishlari bo`yicha",
+      dataIndex: "oquv_ishlari",
+      key: "oquv_ishlari",
+      children: [
         {
-          title: "Baholash turi",
-          key: "Color",
+          title: "15",
+          dataIndex: "oquv_ishlari",
+          key: "oquv_ishlari",
+        },
+      ],
+    },
+    {
+      title: "Yoshlar masalalari bo`yicha",
+      dataIndex: "yoshlar",
+      key: "yoshlar",
+      children: [
+        {
+          title: "15",
+          dataIndex: "yoshlar",
+          key: "yoshlar",
+        },
+      ],
+    },
+    {
+      title: "Ishlab chiqarish ta`limi bo`yicha",
+      dataIndex: "ishlab_chiqarish",
+      key: "ishlab_chiqarish",
+      children: [
+        { title: "15", dataIndex: "ishlab_chiqarish", key: "ishlab_chiqarish" },
+      ],
+    },
+    {
+      title: "Moliyaviy ishalari bo`yicha",
+      dataIndex: "moliyaviy",
+      key: "moliyaviy",
+      children: [{ title: "15", dataIndex: "moliyaviy", key: "moliyaviy" }],
+    },
+    {
+      title: "Xo'jalik ishlari bo`yicha",
+      dataIndex: "xojalik",
+      key: "xojalik",
+      children: [{ title: "10", dataIndex: "xojalik", key: "xojalik" }],
+    },
+    {
+      title:
+        "Talim sifatini nazarat kilish, Horijiy tillarni ommalashtirish, innovasiyalar masalalari ",
+      dataIndex: "talim_sifati",
+      key: "talim_sifati",
+      children: [
+        { title: "20", dataIndex: "talim_sifati", key: "talim_sifati" },
+      ],
+    },
+    {
+      title: "Ijro intizomi, kadrlar masalasi va boshqa masalalar",
+      dataIndex: "ijro_intizomi",
+      key: "ijro_intizomi",
+      children: [
+        { title: "10", dataIndex: "ijro_intizomi", key: "ijro_intizomi" },
+      ],
+    },
+    {
+      title: "Aniqlangan qoida buzarliklar",
+      dataIndex: "jazo",
+      key: "jazo",
+      children: [{ title: "0", dataIndex: "jazo", key: "jazo" }],
+    },
+    {
+      title: "1 oyda  to`plangan   umumiy ball",
+      key: "umumiy",
+      dataIndex: "",
+      render: (text) => {
+        return <p>{parseInt(text.umumiy)} ball</p>;
+      },
+      children: [{ title: "100", dataIndex: "umumiy", key: "umumiy" }],
+    },
+    {
+      title: "Baholash turi",
+      key: "Color",
+      dataIndex: "",
+      children: [
+        {
+          title: "100.0",
           dataIndex: "",
-          children: [
-            {
-              title: "100.0",
-              dataIndex: "",
-              key: "Color",
-              render: (text) => {
-                if (text.umumiy < 50) {
-                  return <p className="red_error">Qoniqarsiz</p>;
-                } else if (text.umumiy > 80) {
-                  return <p className="alo_error">A`lo</p>;
-                } else if (text.umumiy < 80 || text.umumiy > 50) {
-                  return <p className="blue_error">Yaxshi</p>;
-                }
-              },
-            },
-          ],
-        }
-      ];
+          key: "Color",
+          render: (text) => {
+            if (text.umumiy < 50) {
+              return <p className="red_error">Qoniqarsiz</p>;
+            } else if (text.umumiy > 80) {
+              return <p className="alo_error">A`lo</p>;
+            } else if (text.umumiy < 80 || text.umumiy > 50) {
+              return <p className="blue_error">Yaxshi</p>;
+            }
+          },
+        },
+      ],
+    },
+  ];
   return (
     <div className={style.Reyting}>
-        {!data.length ? <h2 className={style.Titlteh2}>Hozircha Reyting joylanmadi!</h2> : 
+      {!data.length ? (
+        <h2 className={style.Titlteh2}>Hozircha Reyting joylanmadi!</h2>
+      ) : (
         <>
-         <h1>{t("reyting")}</h1>
-        <Content style={{ margin: "0 16px" }}>
-        <Table columns={columns} dataSource={data} >
-     
-        </Table>
-       </Content> </>
-        }
-       
-      
+          <h1>{t("reyting")}</h1>
+          <Content style={{ margin: "0 16px" }}>
+            <Table columns={columns} dataSource={data}></Table>
+          </Content>{" "}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Reyting
+export default Reyting;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {  HashRouter } from 'react-router-dom';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
     <HashRouter>
-    
     <App />
     </HashRouter>
 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
